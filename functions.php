2137ac19b8ca93f788908aa1f8f4d1697c9fc25b
Chris Hilsdon <chrish@xigen.co.uk>
2019-07-25 09:51:25 +0100

    Removed require composer autoload.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /**
 * Adds composer autoload file and starts Timber
 ============================================== */
-require_once __DIR__ . '/vendor/autoload.php';
+
 $timber = new \Timber\Timber();
 Timber::$dirname = array( 'templates', 'views' );
 
